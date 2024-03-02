83772213a82a098f7464640a0294a8428b907036
alexL <aleksandrlambert@gmail.com>
2024-03-02 15:55:52 +0700

    первые наброски
    использовалась библиотека
    https://wp-kama.ru/

---
+++ auto/functions.php
@@ -0,0 +1,37 @@
+<?php 
+
+// правильный способ подключить стили и скрипты
+add_action( 'wp_enqueue_scripts', function() {
+
+	wp_enqueue_style( 'gstatic', 'https://fonts.gstatic.com' );
+	wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap' );
+	wp_enqueue_style( 'reset', get_template_directory_uri() . '/assets/css/reset.css' );
+	wp_enqueue_style( 'slick', get_template_directory_uri() . '/assets/css/slick.css' );
+	wp_enqueue_style( 'animate', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' );
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/style.css' );
+
+
+    wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+	wp_enqueue_script( 'jquery' );
+
+
+	wp_enqueue_script( 'slick', get_template_directory_uri() . '/assets/js/slick.min.js', array('jquery'), 'null', true );
+	wp_enqueue_script( 'slick', get_template_directory_uri() . '/assets/js/slick.min.js', array('jquery'), 'null', true );
+	wp_enqueue_script( 'wow', get_template_directory_uri() . '/assets/js/wow.min.js', array('jquery'), 'null', true );
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), 'null', true );
+});
+
+
+
+
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'custom-logo');
+
+
+
+
+
+?>
+
